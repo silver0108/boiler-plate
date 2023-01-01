fefec3eef7be3a39f0fedf3b5eab7e01c4c6cdca
soeunkim0108 <kimsoeun0108@gmail.com>
2023-01-02 01:04:03 +0900

    add: Redux

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import 'antd';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStoreWithMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        Window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
